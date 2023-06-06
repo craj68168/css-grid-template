3e95747ae13a35ca2b5a0e7a737ecc7ea3e269b5
Raj Chaudhary <craj68168@gmail.com>
2023-06-06 17:38:30 +0545

    feat:grid template areas implemented

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,42 @@ const Wrapper = styled.div`
   .grid-container {
     display: grid;
     grid-template-areas:
-      "header"
-      "menu "
-      "main"
-      "right"
-      "footer";
+      "one"
+      "two "
+      "three"
+      "four"
+      "five";
     gap: 10px;
     background-color: #2196f3;
     padding: 10px;
+    grid-auto-colums: 1fr;
+  }
+
+  @media (min-width: 50em) {
+    .grid-container {
+      display: grid;
+      grid-template-areas:
+        "one one two five"
+        "three four four five";
+    }
+  }
+  .container {
+    min-height: 10rem;
+  }
+  .container:nth-child(1) {
+    grid-area: one;
+  }
+  .container:nth-child(2) {
+    grid-area: two;
+  }
+  .container:nth-child(3) {
+    grid-area: three;
+  }
+  .container:nth-child(4) {
+    grid-area: four;
+  }
+  .container:nth-child(5) {
+    grid-area: five;
   }
   .item {
     background-color: rgba(255, 255, 255, 0.8);
@@ -20,32 +48,16 @@ const Wrapper = styled.div`
     padding: 20px 0;
     font-size: 30px;
   }
-
-  .grid-container:nth-child(1) {
-    grid-area: header;
-  }
-  .grid-container:nth-child(2) {
-    grid-area: menu;
-  }
-  .grid-container:nth-child(3) {
-    grid-area: main;
-  }
-  .grid-container:nth-child(4) {
-    grid-area: right;
-  }
-  .grid-container:nth-child(5) {
-    grid-area: footer;
-  }
 `;
 const Home = () => {
   return (
     <Wrapper>
       <div className="grid-container">
-        <div className="item">Header</div>
-        <div className="item">Menu</div>
-        <div className="item">Main</div>
-        <div className="item">Right</div>
-        <div className="item">Footer</div>
+        <div className="container item">One</div>
+        <div className="container item">Two</div>
+        <div className="container item">Three</div>
+        <div className="container item">Four</div>
+        <div className="container item">Five</div>
       </div>
     </Wrapper>
   );
