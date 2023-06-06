18a778328084682304cfcf69c24bb24492af9c06
Raj Chaudhary <craj68168@gmail.com>
2023-06-06 22:29:01 +0545

    feat:grid template areas implemented successfully

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ const Wrapper = styled.div`
     display: grid;
     grid-template-areas:
       "one"
-      "two "
+      "two"
       "three"
       "four"
       "five";
@@ -14,6 +14,32 @@ const Wrapper = styled.div`
     background-color: #2196f3;
     padding: 10px;
     grid-auto-colums: 1fr;
+    .container {
+      min-height: 5rem;
+      display: grid;
+      align-items: center;
+    }
+    .container:nth-child(1) {
+      grid-area: one;
+    }
+    .container:nth-child(2) {
+      grid-area: two;
+    }
+    .container:nth-child(3) {
+      grid-area: three;
+    }
+    .container:nth-child(4) {
+      grid-area: four;
+    }
+    .container:nth-child(5) {
+      grid-area: five;
+    }
+    .item {
+      background-color: rgba(255, 255, 255, 0.8);
+      text-align: center;
+      padding: 20px 0;
+      font-size: 30px;
+    }
   }
 
   @media (min-width: 50em) {
@@ -22,31 +48,12 @@ const Wrapper = styled.div`
       grid-template-areas:
         "one one two five"
         "three four four five";
+      .container {
+        min-height: 10rem;
+        display: grid;
+        align-items: center;
+      }
     }
-  }
-  .container {
-    min-height: 10rem;
-  }
-  .container:nth-child(1) {
-    grid-area: one;
-  }
-  .container:nth-child(2) {
-    grid-area: two;
-  }
-  .container:nth-child(3) {
-    grid-area: three;
-  }
-  .container:nth-child(4) {
-    grid-area: four;
-  }
-  .container:nth-child(5) {
-    grid-area: five;
-  }
-  .item {
-    background-color: rgba(255, 255, 255, 0.8);
-    text-align: center;
-    padding: 20px 0;
-    font-size: 30px;
   }
 `;
 const Home = () => {
