e27ed501c8eada64b5d1995bd3ee5bbb7b1b442e
Raj Chaudhary <craj68168@gmail.com>
2023-09-27 12:32:08 +0545

    feat:refactor codes

---
+++ src/app/Wrapper.tsx
@@ -0,0 +1,58 @@
+"use client";
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  .grid-container {
+    display: grid;
+    grid-template-areas:
+      "one"
+      "two"
+      "three"
+      "four"
+      "five";
+    gap: 10px;
+    background-color: #2196f3;
+    padding: 10px;
+    grid-auto-columns: 1fr;
+    .container {
+      min-height: 5rem;
+      display: grid;
+      align-items: center;
+    }
+    .container:nth-child(1) {
+      grid-area: one;
+    }
+    .container:nth-child(2) {
+      grid-area: two;
+    }
+    .container:nth-child(3) {
+      grid-area: three;
+    }
+    .container:nth-child(4) {
+      grid-area: four;
+    }
+    .container:nth-child(5) {
+      grid-area: five;
+    }
+    .item {
+      background-color: rgba(255, 255, 255, 0.8);
+      text-align: center;
+      padding: 20px 0;
+      font-size: 30px;
+    }
+  }
+
+  @media (min-width: 50em) {
+    .grid-container {
+      display: grid;
+      grid-template-areas:
+        "one one two five"
+        "three four four five";
+      .container {
+        min-height: 10rem;
+        display: grid;
+        align-items: center;
+      }
+    }
+  }
+`;
